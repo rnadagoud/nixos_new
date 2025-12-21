24e956df9f71be1260dbba8706310dc7f85983c3
Rahul Nadagoud <nadagoud@pm.me>
2025-12-21 16:04:30 +0530

    test

--- home.nix
+++ home.nix
@@ -15,6 +15,41 @@
     };
   };
 
+  # Override Hyprland settings
+  wayland.windowManager.hyprland = {
+    enable = true;
+    
+    settings = {
+      # Monitor configuration
+      monitor = [
+        "Virtual-1,1920x1080,0x0,1"
+        # or "DP-1,preferred,auto,1"
+      ];
+      
+      # Input settings (inverted scrolling)
+      input = {
+        touchpad = {
+          natural_scroll = true;  # Inverted/natural scrolling
+        };
+        sensitivity = 0;
+      };
+      
+      # Cursor theme
+      exec-once = [
+        "hyprctl setcursor Bibata-Modern-Ice 24"
+      ];
+    };
+    
+    # Additional custom config that doesn't fit in settings
+    extraConfig = ''
+      # Any raw Hyprland config here
+      input {
+        natural_scroll = yes
+      }
+    '';
+  };
+
+
   home.username = "rnadagoud";
   home.homeDirectory = "/home/rnadagoud";
   home.stateVersion = "25.11";
